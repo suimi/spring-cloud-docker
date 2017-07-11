8e7466fc1687e9f8ff81753d4a2b110af9cac05e
suimi <lichengcai125@gmail.com>
2017-07-11 17:53:29 +0800

    新增turbine zipkin hystrix

---
+++ zipkin-server/src/main/java/com/suimi/hello/zipkin/ZipkinServerApplication.java
@@ -0,0 +1,17 @@
+package com.suimi.hello.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import zipkin.server.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableZipkinServer
+@EnableDiscoveryClient
+public class ZipkinServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinServerApplication.class, args);
+    }
+}
